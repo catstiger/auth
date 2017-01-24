7bc2a2b0fcda6ac97cc4d0f8ffe8020f154a8982
catstiger <catstiger@192.168.0.229>
2017-01-24 18:14:38 +0800

    继续优化代码

--- src/main/java/com/github/catstiger/cache/JsonRedisSerializer.java
+++ src/main/java/com/github/catstiger/cache/JsonRedisSerializer.java
@@ -6,7 +6,6 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
-import org.springframework.util.Assert;
 import org.springframework.util.ClassUtils;
 
 import com.alibaba.fastjson.JSON;
@@ -15,7 +14,6 @@ import com.github.catstiger.utils.StringUtils;
 import com.google.common.base.Strings;
 
 public class JsonRedisSerializer implements RedisSerializer<Object> {
-  private static int MAX_LENGTH_CLASSNAME = 120;
   private static int LENGTH_CLASSNAME_SIZE = 4;
   
   private static Map<String, Class<?>> classCache = new ConcurrentHashMap<>(32);
@@ -24,15 +22,30 @@ public class JsonRedisSerializer implements RedisSerializer<Object> {
     
   @Override
   public byte[] serialize(Object t) throws SerializationException {
-    Assert.notNull(t);
-    
+    if(t == null) {
+      return GenericsJsonRedisSerializer.EMPTY_ARRAY;
+    }
     String json = JSON.toJSONString(t);
-    return serialize(t.getClass(), json);
+    
+    String classname = t.getClass().getName();
+    StringBuilder strBuilder = new StringBuilder(json.length() + LENGTH_CLASSNAME_SIZE + classname.length());
+    strBuilder.append(Strings.padEnd(String.valueOf(classname.length()), LENGTH_CLASSNAME_SIZE, ' '));
+    strBuilder.append(classname);
+    strBuilder.append(json);
+    
+    return strBuilder.toString().getBytes(charset);
   }
 
   @Override
   public Object deserialize(byte[] bytes) throws SerializationException {
-    return parse(bytes);
+    if(bytes == null || bytes.length == 0) {
+      return null;
+    }
+    
+    int clznameLen = Integer.parseInt(StringUtils.trim(new String(bytes, 0, LENGTH_CLASSNAME_SIZE, charset)));
+    String classname = new String(bytes, LENGTH_CLASSNAME_SIZE, clznameLen, charset);
+    Class<?> clazz = loadClass(classname);
+    return JSON.parseObject(bytes, LENGTH_CLASSNAME_SIZE + clznameLen, bytes.length - LENGTH_CLASSNAME_SIZE - clznameLen, ThreadLocalCache.getUTF8Decoder(), clazz);
   }
   
   private Class<?> loadClass(String classname) {
@@ -53,32 +66,5 @@ public class JsonRedisSerializer implements RedisSerializer<Object> {
     }
   }
   
-  private byte[] serialize(Class<?> clazz, String json) {
-    Assert.notNull(clazz);
-    
-    StringBuilder strBuilder = new StringBuilder(json.length() + MAX_LENGTH_CLASSNAME);
-    String classname = clazz.getName();
-    strBuilder.append(Strings.padEnd(String.valueOf(classname.length()), LENGTH_CLASSNAME_SIZE, ' '));
-    strBuilder.append(classname);
-    strBuilder.append(json);
-    
-    return strBuilder.toString().getBytes(charset);
-  }
-  
-  private Object parse(byte[] bytes) {
-    if(bytes == null || bytes.length == 0) {
-      return null;
-    }
-    
-    int clznameLen = Integer.parseInt(StringUtils.trim(new String(bytes, 0, LENGTH_CLASSNAME_SIZE, charset)));
-    String classname = new String(bytes, LENGTH_CLASSNAME_SIZE, clznameLen, charset);
-    Class<?> clazz = loadClass(classname);
-    return JSON.parseObject(bytes, LENGTH_CLASSNAME_SIZE + clznameLen, bytes.length - LENGTH_CLASSNAME_SIZE - clznameLen, ThreadLocalCache.getUTF8Decoder(), clazz);
-  }
-  
-  public static void main(String[]args) {
-    String str = JsonRedisSerializer.class.getName() + "        ";
-    System.out.println(str.length() + "  " + str.getBytes(Charset.forName("UTF-8")).length);
-  }
-  
+ 
 }
